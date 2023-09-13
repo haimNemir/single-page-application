d5deb4abbb05cd433fd70c517c017cdab8999f82
haim nemir <chimnem@gmail.com>
2023-09-13 19:16:18 +0300

    Updates

---
+++ index.7ae1926a.js
@@ -0,0 +1,3 @@
+const e=document.querySelectorAll(".collapsible");e.forEach(e=>e.addEventListener("click",function(){this.classList.toggle("collapsible--expanded")}));//# sourceMappingURL=index.7ae1926a.js.map
+
+//# sourceMappingURL=index.7ae1926a.js.map
